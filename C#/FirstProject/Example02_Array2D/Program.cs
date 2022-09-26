3ea236953e4d5b37ca785ac8806a33e844b6512e
kmw7691 <101630746+kmw7691@users.noreply.github.com>
2022-09-26 18:46:57 +0900

    0926_간단한2d게임 마무리

--- C#/FirstProject/Example02_Array2D/Program.cs
+++ C#/FirstProject/Example02_Array2D/Program.cs
@@ -4,6 +4,10 @@ namespace Example02_Array2D
 {
     class Program
     {
+       // static int[,,,] cube = new int[3, 4, 5];
+           
+     
+
         static int[,] map = new int[5, 5]
         {
             {0, 0, 0, 0, 1},
@@ -15,6 +19,23 @@ namespace Example02_Array2D
 
         static void Main(string[] args)
         {
+            //int count = 0;
+
+            //for (int i = 0; i < cube.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < cube.GetLength(1); j++)
+            //    {
+            //        for (int k = 0; k < cube.GetLength(2); k++)
+            //        {
+            //            cube[i, j, k] = count;
+            //            Console.WriteLine(cube[k,j,i]);
+            //            count++;
+            //        }
+            //        count += 100;
+            //    }
+            //    count += 10000;
+            //}
+
             Player player = new Player(3,0);
             map[0, 3] = 2;
 
@@ -24,20 +45,20 @@ namespace Example02_Array2D
 
                 switch (input)
                 {
-                    case "MoveLeft":
+                    case "L":
                         player.MoveLeft(map);
                         break;
 
-                    case "MoveRight":
-                        player.MoveRight();
+                    case "R":
+                        player.MoveRight(map);
                         break;
 
-                    case "MoveUp":
-                        player.MoveUp();
+                    case "U":
+                        player.MoveUp(map);
                         break;
 
-                    case "MoveDown":
-                        player.MoveDown();
+                    case "D":
+                        player.MoveDown(map);
                         break;
 
                     default:
@@ -81,10 +102,10 @@ namespace Example02_Array2D
             public void MoveLeft(int[,] map)
             {
                 // 맵 범위를 넘어가는지 체크
-                if(_x-1 <0)
+                if(_x - 1 < 0)
                     Console.WriteLine($"플레이어 좌측이동 불가(경계초과), 현재위치 : {_x},{_y}");
                 
-                else if(map[_y,_x-1] != 0)
+                else if(map[_y, _x - 1] != 0)
                     Console.WriteLine($"플레이어 좌측이동 불가(길이없음), 현재위치 : {_x},{_y}");
                 
                 else
@@ -94,12 +115,12 @@ namespace Example02_Array2D
                     Console.WriteLine($"플레이어 좌측으로 한칸 이동. 현재위치 : {_x},{_y}");
                 }    
             }
-            public void MoveRight()
+            public void MoveRight(int[,] map)
             {
-                if (_x + 1 > 4 )
+                if (_x + 1 > map.GetLength(1) - 1)
                     Console.WriteLine($"플레이어 우측이동 불가(경계초과), 현재위치 : {_x},{_y}");
 
-                else if (map[_y, _x - 1] != 0)
+                else if (map[_y, _x + 1] != 0)
                     Console.WriteLine($"플레이어 우측이동 불가(길이없음), 현재위치 : {_x},{_y}");
 
                 else
@@ -109,37 +130,37 @@ namespace Example02_Array2D
                     Console.WriteLine($"플레이어 우측으로 한칸 이동. 현재위치 : {_x},{_y}");
                 }
             }
-            public void MoveUp()
+            public void MoveDown(int[,] map)
             {
-                if (_y + 1 > 4)
-                    Console.WriteLine($"플레이어 상측이동 불가(경계초과), 현재위치 : {_x},{_y}");
+                if (_y + 1 > map.GetLength(0) - 1)
+                    Console.WriteLine($"플레이어 하측이동 불가(경계초과), 현재위치 : {_x},{_y}");
 
                 else if (map[_y + 1, _x] != 0)
-                    Console.WriteLine($"플레이어 상측이동 불가(길이없음), 현재위치 : {_x},{_y}");
+                    Console.WriteLine($"플레이어 하측이동 불가(길이없음), 현재위치 : {_x},{_y}");
 
                 else
                 {
                     map[_y++, _x] = 0;
                     map[_y, _x] = 2;
-                    Console.WriteLine($"플레이어 상측으로 한칸 이동. 현재위치 : {_x},{_y}");
+                    Console.WriteLine($"플레이어 하측으로 한칸 이동. 현재위치 : {_x},{_y}");
                 }
             }
-            public void MoveDown()
+
+            public void MoveUp(int[,] map)
             {
                 if (_y - 1 < 0)
-                    Console.WriteLine($"플레이어 하측이동 불가(경계초과), 현재위치 : {_x},{_y}");
+                    Console.WriteLine($"플레이어 상측이동 불가(경계초과), 현재위치 : {_x},{_y}");
 
                 else if (map[_y - 1, _x] != 0)
-                    Console.WriteLine($"플레이어 하측이동 불가(길이없음), 현재위치 : {_x},{_y}");
+                    Console.WriteLine($"플레이어 상측이동 불가(길이없음), 현재위치 : {_x},{_y}");
 
                 else
                 {
                     map[_y--, _x] = 0;
                     map[_y, _x] = 2;
-                    Console.WriteLine($"플레이어 하측으로 한칸 이동. 현재위치 : {_x},{_y}");
+                    Console.WriteLine($"플레이어 상측으로 한칸 이동. 현재위치 : {_x},{_y}");
                 }
             }
         }
-
     }
 }
