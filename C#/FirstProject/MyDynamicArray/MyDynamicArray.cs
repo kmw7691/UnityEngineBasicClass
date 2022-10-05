d205d528b9b4bcecdb9fae4d960692669987d4db
kmw7691 <101630746+kmw7691@users.noreply.github.com>
2022-10-05 19:43:35 +0900

    1005_enum,enumerator

--- C#/FirstProject/MyDynamicArray/MyDynamicArray.cs
+++ C#/FirstProject/MyDynamicArray/MyDynamicArray.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace MyDynamicArray
 {
-    class MyDynamicArray
+    public class MyDynamicArray<T> : IEnumerable<T>
     {
         private const int DEFAULT_SIZE = 1;
-        private int[] _data = new int[DEFAULT_SIZE];
+        private T[] _data = new T[DEFAULT_SIZE];
 
-        private int this[int index]
+        public T this[int index]
         {
             get
             {
@@ -25,15 +26,18 @@ namespace MyDynamicArray
         }
 
         public int Count;
-        public int Capacity;
 
-        public void Add(int item)
+        //캡슐화
+        public int Capacity => _data.Length;    //get 접근자의 람다식 표현, 람다식 : 익명함수 표현방법
+
+
+        public void Add(T item)
         {
             //배열의 크기가 모자라면
-            if(Count >= Capacity)
+            if (Count >= Capacity)
             {
                 //2배짜리 새로운 배열 생성
-                int[] tmp = new int[Capacity * 2];
+                T[] tmp = new T[Capacity * 2];
 
                 //기존 데이터를 새로운 배열에 복제
                 for (int i = 0; i < Count; i++)
@@ -49,6 +53,11 @@ namespace MyDynamicArray
             Count++;
         }
 
+        //특정 인덱스에 있는 데이터를 지우고
+        //그 뒤 모든 인덱스 데이터들을 한칸씩 앞으로 당겨줌
+
+        //<param name="index">지우고싶은 데이터의 인덱스</param>
+        //<returns>true : 삭제됨, false : 삭제안됨</retruns>
         public bool RemoveAt(int index)
         {
             for (int i = index; i < Count - 1; i++)
@@ -56,34 +65,103 @@ namespace MyDynamicArray
                 _data[i] = _data[i + 1];
             }
             Count--;
-            _data[Count] = 0;
+            _data[Count] = default(T);
 
             return true;
         }
 
-        private int _hp;
+        public bool Remove(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (Comparer<T>.Default.Compare(_data[i], item)== 0)
+                    return RemoveAt(i);
+            }
+            return false;
+        }
 
-        public int Hp
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new MyDynamicArrayEnum<T>(_data, this);
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return new MyDynamicArrayEnum<T>(_data, this);
+        }
+    }
+
+    public class MyDynamicArrayEnum<T> : IEnumerator<T>
+    {
+        private readonly T[] _data;
+        private readonly MyDynamicArray<T> _dynamicArray;
+        private int _index = -1;
+        private int _count => _dynamicArray.Count;
+
+        public T Current
         {
             get
             {
-                return _hp;
-            }
-
-            set
-            {
-                if (value < 0)
-                    value = 0;
-                _hp = value;
-                _hpBar.value = _hp / _hpMax;
+                return _data[_index];
             }
         }
 
-        private UISlider _hpBar;
-        private int _hpMax;
 
-        public int Mp { get; private set; }
+        object IEnumerator.Current => Current;
 
-        class UISlider { public float value; }
+        public MyDynamicArrayEnum(T[] data, MyDynamicArray<T> dynamicArray)
+        {
+            _data = data;
+            _dynamicArray = dynamicArray;
+        }
+
+        public void Dispose()
+        {
+            //nothing to do
+        }
+
+        public bool MoveNext()
+        {
+            _index++;
+            return _index >= 0 && _index < _count;
+        }
+
+        public void Reset()
+        {
+            _index = -1;
+        }
     }
 }
+
+
+
+
+
+
+
+//        private int _hp;
+
+//        public int Hp
+//        {
+//            get
+//            {
+//                return _hp;
+//            }
+
+//            set
+//            {
+//                if (value < 0)
+//                    value = 0;
+//                _hp = value;
+//                _hpBar.value = _hp / _hpMax;
+//            }
+//        }
+
+//        private UISlider _hpBar;
+//        private int _hpMax;
+
+//        public int Mp { get; private set; }
+
+//        class UISlider { public float value; }
+//    }
+//}
--- C#/FirstProject/MyDynamicArray/Program.cs
+++ C#/FirstProject/MyDynamicArray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyDynamicArray
 {
@@ -6,7 +7,47 @@ namespace MyDynamicArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Remove(1);
+            Console.WriteLine(list[0]);
+
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            //foreach문 구현 원리
+            using (IEnumerator<int> enumerator = list.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    Console.WriteLine(enumerator.Current);
+                }
+            }
+            
+
+
+            MyDynamicArray<int> dynamicArray = new MyDynamicArray<int>();
+            dynamicArray.Add(1);
+            dynamicArray.Add(2);
+            dynamicArray.Add(3);
+            dynamicArray.Add(4);
+            dynamicArray.Remove(1);
+            Console.WriteLine(dynamicArray[0]);
+
+            for (int i = 0; i < dynamicArray.Count; i++)
+            {
+                Console.WriteLine(dynamicArray[i]);
+            }
+
+            foreach(int item in dynamicArray)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
