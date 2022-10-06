c3f5cb01136f989dc942e67cb0901f75b60f4f20
kmw7691 <101630746+kmw7691@users.noreply.github.com>
2022-10-06 19:51:05 +0900

    1006_linkedlist

--- C#/FirstProject/MyLinkedList/Program.cs
+++ C#/FirstProject/MyLinkedList/Program.cs
@@ -6,7 +6,16 @@ namespace MyLinkedList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyLinkedList<float> list = new MyLinkedList<float>();
+
+            list.AddFirst(3.0f);
+            list.AddLast(5.0f);
+            list.AddAfter(list.First, 4.0f);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
