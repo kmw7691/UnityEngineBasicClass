58591a0120cff595635b73f58481549c71ceac13
kmw7691 <101630746+kmw7691@users.noreply.github.com>
2022-10-13 19:52:36 +0900

    1013_aaa

--- C#/FirstProject/SortingArgorithm/Program.cs
+++ C#/FirstProject/SortingArgorithm/Program.cs
@@ -7,7 +7,8 @@ namespace SortingArgorithm
         static void Main(string[] args)
         {
             int[] array = { 5, 2, 3, 7, 1, 10, 4, 6, 9, 8 };
-            BubbleSort.Sort(array);
+            //BubbleSort.Sort(array);
+            HeapSort.Sort(array);
 
             for (int i = 0; i < array.Length; i++)
             {
