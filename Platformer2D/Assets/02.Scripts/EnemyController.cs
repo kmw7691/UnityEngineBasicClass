e7ca923239958e5b745711b67511f80c33d6be88
kmw7691 <101630746+kmw7691@users.noreply.github.com>
2022-11-15 19:53:00 +0900

    1115_Platformer2D_Update
    Slug - Add animation

--- Platformer2D/Assets/02.Scripts/EnemyController.cs
+++ Platformer2D/Assets/02.Scripts/EnemyController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Build;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class EnemyController : MonoBehaviour
 {
@@ -15,21 +18,192 @@ public class EnemyController : MonoBehaviour
 
     public States Current;
 
+    private int _idleStep;
+    private int _moveStep;
+    private int _hurtStep;
+    private int _dieStep;
+    private int _attackStep;
+
+    //AI
+    public enum AI
+    {
+        Idle,
+        Think,
+        TakeARest,
+        MoveLeft,
+        MoveRight,
+        FollowTarget,
+        AttackTarget
+    }
+
+    [SerializeField] private AI _ai;
+    [SerializeField] private bool _aiAutoFollow = false;
+    [SerializeField] private float _aiDetectRange = 2.0f;
+    [SerializeField] private bool _aiAttackEnable = false;
+    [SerializeField] private float _aiAttackRange = 0.5f;
+    [SerializeField] private float _aiBehaviorTimeMin = 0.1f;
+    [SerializeField] private float _aiBehaviorTimeMax = 2.0f;
+    private float _aiBehaviorTimer;
+    [SerializeField] private LayerMask _targetLayer;
+
+    //Movement
+    private const int DIRECTION_LEFT = -1;
+    private const int DIRECTION_RIGHT = 1;
+    private int _direction;
+
+    public int Direction
+    {
+        get
+        {
+            return _direction;
+        }
+
+        set
+        {
+            if (value > 0)
+            {
+                _direction = DIRECTION_RIGHT;
+                transform.eulerAngles = Vector3.zero;
+            }
+            else if (value < 0)
+            {
+                _direction = DIRECTION_LEFT;
+                transform.eulerAngles = new Vector3(0.0f, 180.0f, 0.0f);
+            }
+        }
+
+    }
+    [SerializeField] private bool _moveEnable = true;
+    [SerializeField] private bool _movable = false;
+    [SerializeField] private float _movespeed = 1.0f;
+    private Rigidbody2D _rb;
+    private CapsuleCollider2D _col;
+
     //animation
     private Animator _animator;
-    private float _attackAnimTime;
-    private float _hurtAnimTime;
-    private float _dieAnimTime;
-
+    
     private void Awake()
     {
+        _rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        _col = GetComponent<CapsuleCollider2D>();
     }
 
     private void Update()
     {
+        UpdateAI();
         ChangeState(UpdateState());
     }
+
+    private void FixedUpdate()
+    {
+        if(_moveEnable && _movable)
+            _rb.MovePosition(_rb.position + Direction * Vector2.right * _movespeed * Time.fixedDeltaTime);
+    }
+
+    private void UpdateAI()
+    {
+        if(Current == States.Hurt || Current ==States.Die)
+           return;
+
+        Collider2D target = null;
+        switch (_ai)
+        {
+            case AI.Idle:
+                break;
+            case AI.Think:
+                {
+                    target = Physics2D.OverlapCircle(_rb.position, _aiDetectRange, _targetLayer);
+                    if(target != null)
+                    {
+                        if(_aiAttackEnable &&
+                            Vector2.Distance(_rb.position, target.transform.position) < _aiAttackRange)
+                        {
+                            _ai = AI.AttackTarget;
+                            ChangeState(States.Attack);
+                        }
+
+                        else
+                        {
+                            _ai = AI.FollowTarget;
+                            ChangeState(States.Move);
+                        }
+                    }
+
+                    else
+                    {
+                        _ai = (AI)Random.Range((int)AI.TakeARest, (int)AI.MoveRight + 1);
+                        _aiBehaviorTimer = Random.Range(+_aiBehaviorTimeMin, _aiBehaviorTimeMax);
+                        if (_ai == AI.TakeARest)
+                            ChangeState(States.Idle);
+                        else
+                            ChangeState(States.Move);
+                    }
+                }
+                break;
+            case AI.TakeARest:
+                {
+                    if(_aiBehaviorTimer <= 0)
+                    {
+                        _ai = AI.Think;
+                    }
+                    else
+                    {
+                        _aiBehaviorTimer -= Time.deltaTime;
+                    }
+                }
+                break;
+            case AI.MoveLeft:
+                {
+                    if(_aiBehaviorTimer <= 0)
+                    {
+                        _ai = AI.Think;
+                    }
+                    else
+                    {
+                        Direction = DIRECTION_LEFT;
+                        _aiBehaviorTimer -= Time.deltaTime;
+                    }
+                }
+                break;
+            case AI.MoveRight:
+                {
+                    if (_aiBehaviorTimer <= 0)
+                    {
+                        _ai = AI.Think;
+                    }
+                    else
+                    {
+                        Direction = DIRECTION_RIGHT;
+                        _aiBehaviorTimer -= Time.deltaTime;
+                    }
+                }
+                break;
+            case AI.FollowTarget:
+                {
+                    if(_rb.position.x < target.transform.position.x + _col.size.x)
+                    {
+                        Direction = DIRECTION_RIGHT;
+                    }
+                    else if(_rb.position.x > target.transform.position.x - _col.size.x)
+                    {
+                        Direction = DIRECTION_LEFT;
+                    }
+                }
+                break;
+            case AI.AttackTarget:
+                {
+                    if(Current != States.Attack)
+                    {
+                        _ai = AI.Think;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
 
     public bool ChangeState(States newState)
     {
@@ -64,23 +238,29 @@ public class EnemyController : MonoBehaviour
             switch (newState)
             {
                 case States.Idle:
-                    Idle().Reset();
+                    _idleStep = 1;
+                    _movable = false;
                     break;
                 case States.Move:
-                    Move().Reset();
+                    _moveStep = 1;
+                    _movable = true;
                     break;
                 case States.Attack:
-                    Attack().Reset();
+                    _attackStep = 1;
+                    _movable = false;
                     break;
                 case States.Hurt:
-                    Hurt().Reset(); 
+                    _hurtStep = 1;
+                    _movable = false;
                     break;
                 case States.Die:
-                    Die().Reset();
+                    _dieStep = 1;
+                    _movable = false;
                     break;
                 default:
                     break;
             }
+
             Current = newState;
         }
 
@@ -92,19 +272,19 @@ public class EnemyController : MonoBehaviour
         switch (Current)
         {
             case States.Idle:
-                return Idle().Current;
+                return Idle();
                 break;
             case States.Move:
-                return Move().Current;
+                return Move();
                 break;
             case States.Attack:
-                return Attack().Current;
+                return Attack();
                 break;
             case States.Hurt:
-                return Hurt().Current;
+                return Hurt();
                 break;
             case States.Die:
-                return Die().Current;
+                return Die();
                 break;
             default:
                 throw new System.Exception("Wrong State");
@@ -116,10 +296,22 @@ public class EnemyController : MonoBehaviour
         return true;
     }
 
-    private IEnumerator<States> Idle()
+    private States Idle()
     {
-        _animator.Play("Idle");
-        yield return States.Idle;
+        switch (_idleStep)
+        {
+            case 0:
+                // nothing to do
+                break;
+            case 1:
+                _animator.Play("Idle");
+                _idleStep++;
+                break;
+            default:
+                break;
+        }
+
+        return States.Idle;
     }
 
     //Move 실행 가능 조건
@@ -129,27 +321,63 @@ public class EnemyController : MonoBehaviour
     }
 
     //Move상태 로직
-    IEnumerator<States> Move()
+    private States Move()
     {
-        _animator.Play("Move");
-        yield return States.Idle;
+        switch (_moveStep)
+        {
+            case 0:
+                // nothing to do
+                break;
+            case 1:
+                _animator.Play("Move");
+                _moveStep++;
+                break;
+            case 2:
+                // finished
+                break;
+            default:
+                break;
+        }
+
+        return States.Move;
     }
 
+    //Hurt 실행 가능 조건
     private bool CanHurt()
     {
         return true;
     }
 
-    //Attack상태 로직
-    IEnumerator<States> Hurt()
+    //Hurt상태 로직
+    private States Hurt()
     {
-        _animator.Play("Hurt");
-        while (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+        States next = States.Hurt;
+        switch (_hurtStep)
         {
-            yield return States.Hurt;
+            case 0:
+                // nothing to do
+                break;
+            case 1:
+                _animator.Play("Hurt");
+                _hurtStep++;
+                break;
+            case 2:
+                // finished
+                {
+                    if (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+                    {
+                        _hurtStep++;
+                    }
+                }
+                break;
+            default:
+                {
+                    next = States.Idle;
+                }
+                break;
         }
 
-        yield return States.Idle;
+        return next;
     }
 
     //Attack 가능 조건
@@ -159,32 +387,73 @@ public class EnemyController : MonoBehaviour
     }
 
     //Attack상태 로직
-    IEnumerator<States> Attack()
+    private States Attack()
     {
-        _animator.Play("Attack");
-        while (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+        States next = States.Attack;
+        switch (_attackStep)
         {
-            yield return States.Attack;
+            case 0:
+                // nothing to do
+                break;
+            case 1:
+                _animator.Play("Attack");
+                _attackStep++;
+                break;
+            case 2:
+                // finished
+                {
+                    if(_animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+                    {
+                        _attackStep++;
+                    }
+                }
+                break;
+            default:
+                {
+                    next = States.Idle;
+                }
+                break;
         }
 
-        yield return States.Idle;
+        return next;
     }
 
+    //Die 가능 조건
     private bool CanDie()
     {
         return true;
     }
 
-    //Attack상태 로직
-    IEnumerator<States> Die()
+    //Die상태 로직
+    private States Die()
     {
-        _animator.Play("Die");
-        while (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+        States next = States.Die;
+        switch (_dieStep)
         {
-            yield return States.Die;
+            case 0:
+                // nothing to do
+                break;
+            case 1:
+                _animator.Play("die");
+                _dieStep++;
+                break;
+            case 2:
+                // finished
+                {
+                    if (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+                    {
+                        Destroy(gameObject);
+                        _dieStep++;
+                    }
+                }
+                break;
+            default:
+                {
+                    next = States.Idle;
+                }
+                break;
         }
 
-        Destroy(gameObject);
-        yield return States.Idle;
+        return next;
     }
 }
